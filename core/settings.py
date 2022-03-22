2ab8881422684d94893928d9699e9e766b7f325b
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-22 09:01:34 +0300

    fixed

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://zakharkevichdev.herokuapp.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
