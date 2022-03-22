7529dfd326492bde739337f669b1f6f5128f762a
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-22 11:02:34 +0300

    fixed

--- core/settings.py
+++ core/settings.py
@@ -109,11 +109,11 @@ USE_I18N = True
 USE_TZ = True
 
 ADMINS = [
-    ('', env('EMAIL_HOST_USER')),
+    ('', 'zakharkevich.v@gmail.com'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
@@ -121,7 +121,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
