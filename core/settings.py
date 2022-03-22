40e53d1981c812075a5124e3a3cc8ed8205b031d
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-22 08:28:04 +0300

    fixed

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = env('DEBUG', default=False, cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['zakharkevichdev']
 
