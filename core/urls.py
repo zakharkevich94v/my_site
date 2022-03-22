ff8d901d390a2e58618407f9027bed7ef9b4641d
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-22 09:28:43 +0300

    fixed

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from blog.views import page_not_found_view
 
 
 urlpatterns = [
@@ -11,3 +12,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = page_not_found_view()
