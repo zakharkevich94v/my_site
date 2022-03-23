73eb321cba9d4f546d9df34aa5f05b476bc19ab9
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-23 09:15:33 +0300

    test

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import environ
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
