e253df1e34510ee5a0c363aac13ecb78fc8f430f
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-27 18:32:31 +0300

    test

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/'),]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
