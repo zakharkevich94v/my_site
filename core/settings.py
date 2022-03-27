5248a151e057c53b591bcd0b8885e58e9c717ad0
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-27 18:50:52 +0300

    test

--- core/settings.py
+++ core/settings.py
@@ -105,6 +105,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
+STATICFILES_FINDERS = (
+ 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+ 
+)
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = 'media/'
