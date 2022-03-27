9703c518357370c6553842f419200da63c8d5035
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-27 18:22:34 +0300

    test

--- core/settings.py
+++ core/settings.py
@@ -99,7 +99,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
