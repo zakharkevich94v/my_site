477e700899aae3d14997beb17408badaaca71e0f
zakharkevich94v <zakharkevich.v@gmail.com>
2022-03-27 19:11:04 +0300

    test

--- core/settings.py
+++ core/settings.py
@@ -101,9 +101,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [STATIC_DIR]
 
 # STATICFILES_FINDERS = (
  
